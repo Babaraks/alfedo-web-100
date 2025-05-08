9056699a102d3773c008f923a2ff0ec396c1648d
Babaraks <reyes.reser23@gmail.com>
2025-05-08 17:16:04 -0600

    dw

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  base: '/portafolio/',
+  base: '/alfedo-web-100/',
   plugins: [
     tailwindcss(),
   ],
